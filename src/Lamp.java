82c19368c8aad3bbec84a3af84539b2ec226a155
Omerun <1513finger@gmail.com>
2022-11-15 13:23:08 +0700

    new

---
+++ src/Lamp.java
@@ -0,0 +1,13 @@
+package com.company;
+public class Lamp {
+    // stores the value for light
+    // true if light is on
+    // false if light is off
+    boolean isOn;
+
+    // method to turn on the light
+    void turnOn() {
+        isOn = true;
+        System.out.println("Light on? " + isOn);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // create object here
+        Dog Dog10bject = new Dog();
+        Dog Dog20bject = new Dog();
+        Dog Dog30bject = new Dog();
+
+//        Dog10bject.testDog();
+//        Dog20bject.testDog2();
+//        Dog30bject.testDog3();
+
+
+        // create an object of Lamp
+        Lamp led = new Lamp();
+        // access method using object
+        led.turnOn();
+
+    }
+}
+
+
